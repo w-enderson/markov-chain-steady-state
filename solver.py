ee6f271ffda7e60b16d8f52d10e9429dfaa46be7
wenderson-juvenal <wendersjuv@gmail.com>
2026-04-18 15:21:46 -0300

    implementação da montagem do sistema linear associado ao estado estacionário

---
+++ solver.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+from markov_generators import generate_transition_matrix
+
+def build_steady_state_system(P : np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Seja:
+    - P, a matriz de transição
+
+    Constrói o sistema linear (I - P)^T * pi = b
+
+    adicionando a restrição sum(pi) = 1.
+
+    Args:
+        P (np.ndarray): Matriz de transição (n x n).
+        
+    Returns:
+        tuple: (A, b), 
+        
+        onde A nxn = (I-P).T com primeira linha substituída por vetor de 1's
+             b é nx1 = [1, 0, 0, ..., 0]
+    """
+
+    n= P.shape[0]
+
+    A= (np.eye(n) - P).T
+    b= np.zeros(n)
+
+    A[0, :]= 1.0
+    b[0]= 1.0
+
+    return A, b
+
+
+
+
+if __name__ == "__main__":
+    P= generate_transition_matrix(dim=3, alpha=3)
+    A, b= build_steady_state_system(P)
+
+    print(A, b)
