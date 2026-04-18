680ac3f0b8ccc58d359c758449d8d0aed3a4ebba
wenderson-juvenal <wendersjuv@gmail.com>
2026-04-18 16:45:38 -0300

    implementando decomposição QR

--- solver.py
+++ solver.py
@@ -20,7 +20,7 @@ def build_steady_state_system(P : np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         onde A nxn = (I-P).T com primeira linha substituída por vetor de 1's
              b é nx1 = [1, 0, 0, ..., 0]
     """
-
+    
     n= P.shape[0]
 
     A= (np.eye(n) - P).T
@@ -32,10 +32,46 @@ def build_steady_state_system(P : np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     return A, b
 
 
+def qr_decomposition(A : np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """
+        Decomposição QR da matriz A (nxn) via reflexões de Householder
+    """
+    n, m = A.shape
+    R = A.copy().astype(float)
+    Q = np.eye(n)
+
+
+    for i in range(n):
+        
+        x = R[i:, i]
+        
+        e1 = np.zeros_like(x)
+        e1[0] = np.linalg.norm(x)
+
+        v = x + np.sign(x[0]) * e1
+
+        if np.linalg.norm(v) < 1e-15:
+            continue
+        
+        # Matriz de householder
+        H_mini = np.eye(len(v)) - 2.0 * np.outer(v, v) / np.dot(v, v)
+
+        H_k = np.eye(n)
+        H_k[i:, i:] = H_mini
+
+        R = H_k @ R
+        Q = Q @ H_k
+
+    return Q, R
 
 
 if __name__ == "__main__":
     P= generate_transition_matrix(dim=3, alpha=3)
     A, b= build_steady_state_system(P)
 
-    print(A, b)
+
+    print(A)
+    print(b)
+    Q, R = qr_decomposition(A)
+    print(Q)
+    print(R)
